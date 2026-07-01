2b9dbfba0445b2d924f9a0125be9c53c2ddfc720
yunseonglab-droid <yunseonglab@gmail.com>
2026-07-01 22:25:17 +0900

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,5 +1,5 @@
 // js/firebase.js
-// REM404 Archive v0.4
+// REM404 Archive v0.3.8
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
 
@@ -49,7 +49,7 @@ export async function saveMemory(memoryText) {
     exhibition: "REM404 Archive Test",
     project: "REM404",
     archive: "REM404 Archive",
-    version: "v0.4",
+    version: "v0.3.8",
     language: "ko"
   });
 }
