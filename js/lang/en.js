12c97e243994781f15a60b304d6949198b32da85
yunseonglab-droid <yunseonglab@gmail.com>
2026-07-05 21:46:31 +0900

    Add English language localization for status and buttons

---
+++ js/lang/en.js
@@ -0,0 +1,33 @@
+// js/lang/en.js
+
+export default {
+  status: {
+    alignPhoto: "Align the photo within the frame.",
+    alignPhotoSub: "Make sure the entire photo fits<br>inside the frame.",
+    avoidGlare: "Align the photo within the frame.",
+    avoidGlareSub: "Avoid glare and try again.",
+    targetLost: "Point your camera at the photo again.",
+    targetLostSub: "Make sure the photo fits inside the frame<br>and avoid glare.",
+    memoryFound: "Memory found.",
+    memoryFoundSub: "Restoring memory.",
+    memoryRestored: "Memory restored.",
+    memoryRestoredSub: "Leave a memory this place brings back."
+  },
+
+  buttons: {
+    leaveMemory: "Leave a Memory",
+    submitMemory: "Leave a Memory",
+    viewMemory: "View Another Memory",
+    nextMemory: "View Another Memory",
+    loadingMemory: "Loading memory...",
+    savingMemory: "Saving memory..."
+  },
+
+  archive: {
+    loadFailed: "Could not load memory.",
+    saveFailed: "Could not save memory. Please try again.",
+    systemLoadFailed: "Could not load the memory system. Please try again.",
+    allReadTitle: "You have read all memories.",
+    allReadSub: "Come back when new memories are added."
+  }
+};
